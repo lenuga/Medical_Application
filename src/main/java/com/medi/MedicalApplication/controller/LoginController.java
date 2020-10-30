187b0e914e3abd508d04dffa9756d85b483760d9
lenuga <58283400+lenuga@users.noreply.github.com>
2020-10-30 20:28:36 +0530

    Update LoginController.java

--- src/main/java/com/medi/MedicalApplication/controller/LoginController.java
+++ src/main/java/com/medi/MedicalApplication/controller/LoginController.java
@@ -15,17 +15,5 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/addlogin")
-    public Login addLogin(@RequestBody Login login) { return loginService.saveLogin(login); }
-    @PostMapping("/addLogins")
-    public List<Login> addLogins(@RequestBody List<Login> logins) { return loginService.saveLogins(logins); }
-    @GetMapping("/logins")
-    public List<Login> findAllLogins(){ return loginService.getLogins(); }
-    @GetMapping("/{id}")
-    public Login findByLoginId(@PathVariable (value="id") Long loginId) {return loginService.getLoginByLoginId(loginId);}
-    @GetMapping("/login/{name}")
-    public Login findByUsername(@PathVariable(value = "name") String username) {return loginService.getLoginByUsername(username); }
-    @PutMapping("/update")
-    public Login updateLogin(@RequestBody Login login){ return loginService.updateLogin(login); }
 
 }
