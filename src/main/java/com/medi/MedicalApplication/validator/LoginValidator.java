fcb8889c3466edb6309fc03e19eaca025ac47d84
lenuga <58283400+lenuga@users.noreply.github.com>
2020-11-08 11:14:05 +0530

    Update LoginValidator.java

--- src/main/java/com/medi/MedicalApplication/validator/LoginValidator.java
+++ src/main/java/com/medi/MedicalApplication/validator/LoginValidator.java
@@ -1,4 +1,30 @@
 package com.medi.MedicalApplication.validator;
 
-public class LoginValidator {
+import com.medi.MedicalApplication.model.Login;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class LoginValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Login.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object object, Errors errors) {
+         Login login = (Login) object;
+
+         if(login.getPassword().length() <6){
+             errors.rejectValue("password", "Length", "password must be at least 6 characters");
+         }
+//confirmpassword
+         if(!login.getPassword().equals(login.getConfirmpassword())){
+             errors.rejectValue("confirmpassword", "Length", "password must match");
+
+         }
+    }
 }
